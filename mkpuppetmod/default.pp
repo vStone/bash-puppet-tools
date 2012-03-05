aca445b2c3995d07db62826999c42a11cf293ed7
Jan Vansteenkiste <jan@vstone.eu>
2012-03-05 11:25:35 +0100

    Added my default class template file

---
+++ mkpuppetmod/default.pp
@@ -0,0 +1,24 @@
+# = Class: %class
+#
+# Description of %class
+#
+# == Parameters:
+#
+# $param::   description of parameter. default value if any.
+#
+# == Actions:
+#
+# Describe what this class does. What gets configured and how.
+#
+# == Requires:
+#
+# Requirements. This could be packages that should be made available.
+#
+# == Sample Usage:
+#
+class %class {
+  ## Copy paste snippets:
+  # template("${module_name}/template.erb")
+  # source => "puppet:///modules/${module_name}/file"
+}
+
